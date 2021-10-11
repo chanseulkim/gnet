2c26cecb07cea410bd7f8efd405ca5cf3b54f73b
chanseulkim <31463944+chanseulkim@users.noreply.github.com>
2021-10-11 16:45:51 +0900

    * update

--- packet_sync.go
+++ packet_sync.go
@@ -1,17 +1,17 @@
 package gnet
 
-type SyncPacket struct {
-	header      *GHeader
-	object_type byte
-	buff        []byte
-	buff_len    int32
-}
+// type SyncPacket struct {
+// 	header      *GHeader
+// 	object_type byte
+// 	buff        []byte
+// 	buff_len    int32
+// }
 
-func NewSyncPacket(object_type byte, data []byte, data_size int32) *SyncPacket {
-	return &SyncPacket{
-		header:      NewGHeader(TYPE_HEADER_SYNC),
-		object_type: object_type,
-		buff:        data,
-		buff_len:    data_size,
-	}
-}
+// func NewSyncPacket(object_type byte, data []byte, data_size int32) *SyncPacket {
+// 	return &SyncPacket{
+// 		header:      NewGHeader(TYPE_HEADER_SYNC),
+// 		object_type: object_type,
+// 		buff:        data,
+// 		buff_len:    data_size,
+// 	}
+// }
